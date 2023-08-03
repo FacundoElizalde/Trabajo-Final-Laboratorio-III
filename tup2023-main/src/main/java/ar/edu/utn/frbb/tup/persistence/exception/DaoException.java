af36af42fa78ddbcaa8776a04e2f481be226d40c
Facundo Elizalde <faculobitoelizalde@gmail.com>
2023-08-03 18:53:20 -0300

    DaoException

---
+++ tup2023-main/src/main/java/ar/edu/utn/frbb/tup/persistence/exception/DaoException.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frbb.tup.persistence.exception;
+
+public class DaoException extends Exception {
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
