45cda30ca357c8593f71d11d0bae24dfc855b084
Facundo Elizalde <faculobitoelizalde@gmail.com>
2023-08-03 19:33:01 -0300

    AlumnoService, MateriaService e Impl

---
+++ tup2023-main/src/main/java/ar/edu/utn/frbb/tup/business/impl/MateriaServiceImpl.java
@@ -0,0 +1,43 @@
+package ar.edu.utn.frbb.tup.business.impl;
+
+import ar.edu.utn.frbb.tup.business.MateriaService;
+import ar.edu.utn.frbb.tup.model.Alumno;
+import ar.edu.utn.frbb.tup.model.Materia;
+import ar.edu.utn.frbb.tup.model.dto.MateriaDto;
+import ar.edu.utn.frbb.tup.persistence.MateriaDao;
+import ar.edu.utn.frbb.tup.persistence.MateriaDaoMemoryImpl;
+import ar.edu.utn.frbb.tup.persistence.exception.MateriaNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class MateriaServiceImpl implements MateriaService {
+    @Autowired
+    private MateriaDao dao;
+
+    @Override
+    public Materia crearMateria(MateriaDto materia) throws IllegalArgumentException{
+        Materia m = new Materia();
+        m.setNombre(materia.getNombre());
+        m.setAnio(materia.getAnio());
+        m.setCuatrimestre(materia.getCuatrimestre());
+        dao.save(m);
+        if (m.getNombre().contains("a")) {
+            throw new IllegalArgumentException();
+        }
+        return m;
+    }
+
+    @Override
+    public List<Materia> getAllMaterias() {
+        return null;
+    }
+
+    @Override
+    public Materia getMateriaById(int idMateria) throws MateriaNotFoundException {
+        return dao.findById(idMateria);
+    }
+}
